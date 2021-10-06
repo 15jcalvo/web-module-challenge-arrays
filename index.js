a5f5b57f18d0eb2908328194e0ad555cc2c4ee7b
James_Calvo <15jcalvo@gmail.com>
2021-10-06 16:15:57 -0400

    Task 2 complete

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ Use the copy function below to do the following:
   1. receive an array as a parameter - you will pass in originalFlavors as an argument when the function is invoked.
   2. Return a copy of the received array  
 */
+
 function copy(array){
   let originalCopy = [];
   originalCopy = [...array];
@@ -51,10 +52,6 @@ function copy(array){
   return originalCopy;
 }    
 copy(originalFlavors);
-
-
-
-
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Confirm that an array is exactly 31 flavors. Your function should accept:
@@ -66,10 +63,17 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
-
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(array){
+ if(array.length == 31){
+   x = new Boolean(true);
+ }
+ else{
+   x = new Boolean(false);
+ }
+ console.log(x);
+ return x;
 }
+is31Flavors(originalFlavors);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
